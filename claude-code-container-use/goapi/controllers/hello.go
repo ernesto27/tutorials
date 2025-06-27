6588af08ef59a26bcfb95e8163c17b0879547a94
ernesto <ernestoponce27@gmail.com>
2025-06-27 19:50:06 -0300

    Write claude-code-container-use/goapi/controllers/hello.go
    Creating hello controller with the hello handler

---
+++ claude-code-container-use/goapi/controllers/hello.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello World"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
