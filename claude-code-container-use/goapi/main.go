5f80e12ea9aad4a51fece7a34de5db72c921ba4f
ernesto <ernestoponce27@gmail.com>
2025-06-27 19:06:25 -0300

    add version endpoint and version.json file

--- claude-code-container-use/goapi/main.go
+++ claude-code-container-use/goapi/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 )
 
 type Response struct {
 	Message string `json:"message"`
+}
+
+type VersionResponse struct {
+	Version string `json:"version"`
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +20,25 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func versionHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("version.json")
+	if err != nil {
+		http.Error(w, "Could not read version file", http.StatusInternalServerError)
+		return
+	}
+
+	var versionData VersionResponse
+	if err := json.Unmarshal(data, &versionData); err != nil {
+		http.Error(w, "Could not parse version file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(versionData)
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/version", versionHandler)
 	http.ListenAndServe(":8080", nil)
 }
